9b57f27c319c34b18e27b58aed309423f9674241
Shadrack Udhewe <SUdhewe@polarisbanklimited.com>
2025-02-21 18:11:55 +0100

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 
-let number = 50
-
-if (number % 2 === 0) {
-    console.log("number is Even")
-} else {
-    console.log("number is Odd")
-}
+const friends = ["John", "Peter", "Jane", "david", "luke"]
+console.log(friends)
