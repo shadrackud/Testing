0550a19a5365dc7c069507550e2552c730331198
Shadrack Udhewe <SUdhewe@polarisbanklimited.com>
2025-02-26 12:40:26 +0100

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
-const friends = ["John", "Peter", "Jane", "david", "luke"]
-console.log(friends)
+//const friends = ["John", "Peter", "Jane", "david", "luke"]
+//console.log(friends)
+
+const friends = ["John", "Sarah", "Michael", "Emma", "David"];
+const bestFriend = friends[1];
+console.log("My best friend is:", bestFriend);
