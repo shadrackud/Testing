3488a992023dfce57f20b1fd118e37194c9ab77d
shadrackud <shadrackud@gmail.com>
2025-03-01 15:10:38 +0100

    first commit

---
+++ javatranings.js
@@ -0,0 +1,16 @@
+let num1 = 15;
+let num2 = 4;
+
+// Perform arithmetic operations
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let modulus = num1 % num2;
+
+// Print the results using concatenation
+console.log("Addition: " + num1 + " + " + num2 + " = " + addition);
+console.log("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
+console.log("Multiplication: " + num1 + " * " + num2 + " = " + multiplication);
+console.log("Division: " + num1 + " / " + num2 + " = " + division);
+console.log("Modulus: " + num1 + " % " + num2 + " = " + modulus);
