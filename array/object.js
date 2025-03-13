96d4f7de3c9a801a4c64d4a2336fd51db26c2424
shadrackud <shadrackud@gmail.com>
2025-03-13 23:30:10 +0100

    first commit

---
+++ array/object.js
@@ -0,0 +1,25 @@
+const Books =[
+{
+    title:'power of your mind',
+    description:'an effective tool for the mind',
+    numberOfPages:'123',
+    author:'pastor chris',
+    reading: 'true'
+},
+{
+title:'how to pray effectively',
+description:'getting answeerS to your prayers ',
+numberOfPages:'152',
+author:'pastor chris',
+reading: 'true'
+},
+{
+title:'Rhaspody',
+description:'a daily devotional ',
+numberOfPages:'144',
+author:'pastor chris',
+reading: 'true'
+}
+]
+
+console.log(Books)
